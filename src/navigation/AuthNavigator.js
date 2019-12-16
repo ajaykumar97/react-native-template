e002536093d3fcf530e3250639525a5d7b9273cc
Ajay <ajay@codebrewinnovations.com>
2019-12-16 14:52:05 +0530

    updated react-navigation to version 4

--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -1,4 +1,4 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 export default createStackNavigator(
     {
--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,4 +1,4 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import UsersScreen from '../screens/UsersScreen';
 
