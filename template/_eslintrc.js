22ee73c2f0aaa02022fc7e399c0efff679edd37a
ajaykumar97 <ajaykumar.emilence@gmail.com>
2021-03-27 19:43:39 +0530

    Added `eslint-plugin-import`

--- template/_eslintrc.js
+++ template/_eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ["import"],
+  rules: {
+    // eslint-plugin-import
+    "import/first": 1,
+    "import/newline-after-import": 1,
+    "import/no-cycle": 2,
+    "import/no-duplicates": 1,
+    "import/no-unresolved": 2,
+  }
 };
