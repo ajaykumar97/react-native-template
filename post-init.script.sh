3e3fc25ef61b49ab0f5e487f93d1971a129d7e0a
ajaykumar97 <ajaykumar.emilence@gmail.com>
2021-09-19 20:06:43 +0530

    chore: upgraged `husky` init config

--- post-init.script.sh
+++ post-init.script.sh
@@ -2,9 +2,12 @@
 
 echo "\nInitializing git repository\n"
 git init
+
 echo "\nInitializing husky\n"
 npx husky-init
-echo "\nInitializing node_modules\n"
+
+echo "\nInstalling node_modules\n"
 yarn install
+
 echo "\nInitializing husky hooks\n"
-npx husky add .husky/pre-commit "yarn test && yarn lint"
+npx husky set .husky/pre-commit "yarn test && yarn lint"
