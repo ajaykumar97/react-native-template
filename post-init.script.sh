bebf97dc9147e0d212e937d147ba6239a5aa7b0d
ajaykumar97 <ajaykumar.emilence@gmail.com>
2021-09-19 19:50:50 +0530

    chore: added `git init` in `post-init.script.sh`

--- post-init.script.sh
+++ post-init.script.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-echo "Initializing husky"
+echo "\nInitializing git repository\n"
+git init
+echo "\nInitializing husky\n"
 npx husky-init
+echo "\nInitializing node_modules\n"
 yarn install
+echo "\nInitializing husky hooks\n"
 npx husky add .husky/pre-commit "yarn test && yarn lint"
