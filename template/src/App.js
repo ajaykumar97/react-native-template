3b809d66a4c9a511c1b5ae88918c5fd552b1922a
ajaykumar97 <ajaykumar.emilence@gmail.com>
2021-12-04 22:44:08 +0530

    refactor: renaming `navigationService`

--- template/src/App.js
+++ template/src/App.js
@@ -6,7 +6,7 @@ import FlashMessage from 'react-native-flash-message';
 
 import store from './store';
 import {colors} from './utilities/constants';
-import {navigationRef} from './utilities/NavigationService';
+import {navigationRef} from './utilities/navigationService';
 import {layout} from './utilities/layout';
 import AppNavigator from './navigation/AppNavigator';
 
