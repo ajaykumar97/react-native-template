d55783e33b36adea082f08de1c126dd79639c219
ajaykumar97 <ajaykumar.emilence@gmail.com>
2021-12-19 22:17:39 +0530

    docs: added installation GIF into documentation website

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -5,6 +5,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import { urls } from '../../static/constants';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -13,6 +14,11 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <img
+          style={{height: 400, width: 800}}
+          src={urls.templateInstallationGIF}
+          alt="Logo"
+        />
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
