91f430062abc14c1f8d61d80a77f498a83ca9678
ajaykumar97 <38132295+ajaykumar97@users.noreply.github.com>
2024-06-19 22:50:14 +0530

    fix: lint issues

--- template/scripts/runPostInstallUninstallTasks.js
+++ template/scripts/runPostInstallUninstallTasks.js
@@ -1,11 +1,15 @@
 const exec = require('child_process').exec;
 const os = require('os');
-const { circle } = require('cli-spinners');
+const {circle} = require('cli-spinners');
 
 if (os.type() === 'Darwin') {
   let x = 0;
   const loader = setInterval(() => {
-    process.stdout.write(`\r${circle.frames[x++]} This step will install all the pods for iOS and can take a while, depending on your internet speed. Thank you for your patience.`);
+    process.stdout.write(
+      `\r${
+        circle.frames[x++]
+      } This step will install all the pods for iOS and can take a while, depending on your internet speed. Thank you for your patience.`,
+    );
     x %= circle.frames.length;
   }, circle.interval);
 
